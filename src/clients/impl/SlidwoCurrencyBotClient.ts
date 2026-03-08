dc855b480c82785899f128b5cff641f550588835
Viacheslav Kopeika <38747054+slidvo@users.noreply.github.com>
2026-03-08 19:29:53 +0300

    tmp

--- src/clients/impl/SlidwoCurrencyBotClient.ts
+++ src/clients/impl/SlidwoCurrencyBotClient.ts
@@ -3,29 +3,34 @@ import type { SendMessage } from "../../dto/SendMessage.js";
 import type { EnvironmentService } from "../../services/EnvironmentService.js";
 import type { TelegramBotClient } from "../TelegramBotClient.js";
 
+const TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN";
 export default class SlidwoCurrencyBotClient implements TelegramBotClient {
   constructor(private envService: EnvironmentService) {}
   async sendMessage(msg: SendMessage): Promise<void> {
-    const token = this.envService.get("TELEGRAM_BOT_TOKEN");
+    const token = this.envService.get(TELEGRAM_BOT_TOKEN);
 
     console.log(`DEBUG: sendMessageDto= ${JSON.stringify(msg)} `);
 
-    // const jsonData = JSON.stringify(msg);
+    const jsonData = JSON.stringify(msg);
 
-    // const options: https.RequestOptions = {
-    //   hostname: "example.com",
-    //   port: 443,
-    //   path: `https://api.telegram.org/${token}/sendMessage`,
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "Content-Length": Buffer.byteLength(jsonData),
-    //   },
-    // };
+    const sendMessagePath = `https://api.telegram.org/${token}/sendMessage`;
 
-    // const req = https.request(options, (res) => {});
+    const options: https.RequestOptions = {
+      hostname: "example.com",
+      port: 443,
+      path: sendMessagePath,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(jsonData),
+      },
+    };
 
-    // req.write(jsonData);
-    // req.end();
+    console.log(`DEBUG: sendMessagePath: ${sendMessagePath}`);
+
+    const req = https.request(options, (res) => {});
+
+    req.write(jsonData);
+    req.end();
   }
 }
--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -1,3 +1,3 @@
 export interface EnvironmentService {
-  get(key: string): Promise<string>;
+  get(key: string): string;
 }
