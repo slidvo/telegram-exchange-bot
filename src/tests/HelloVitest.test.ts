a261f1465930d70c9c1f086452f74291e4179ef7
Vyacheslav Kopeyka <38747054+slidvo@users.noreply.github.com>
2026-03-16 09:54:55 +0500

    feature: add vitest

---
+++ src/tests/HelloVitest.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+describe("CurrencyService", () => {
+  it("should return rate for USD/RUB", async () => {
+    expect(1 + 1).toBe(2);
+    console.log("DONE");
+  });
+});
